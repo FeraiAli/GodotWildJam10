bdcc359122a9793802b72dec16487a529ddd1b53
Borislav Dimitrov <borislav.dimitrov@egt-bg.com>
2019-06-14 19:01:41 +0300

    Fixed minor issues with the dialog box and removed the test 'World'

--- Subtitles/Dialog.gd
+++ Subtitles/Dialog.gd
@@ -4,6 +4,7 @@ var dialog = ["This developer made me a happy little land bla bla.",
 	"I see so many glitches , cant this guy make anything right.",
 	"I need to do something or else I will be killed!"]
 var page = 0
+var skippedDialog = false
 
 func _ready():
 	set_bbcode(dialog[page])
@@ -13,22 +14,29 @@ func _ready():
 func _input(event):
 	if event.is_action_pressed("skip_dialog"):
 		if get_visible_characters() > get_total_character_count():
-			if page < dialog.size() - 1:
-				page += 1
-				set_bbcode(dialog[page])
-				set_visible_characters(0)
+			CheckForEndOfDialog()
+			skippedDialog = false
 		else:
 			set_visible_characters(get_total_character_count())
+			skippedDialog = true
 
 func _on_VisibleCharacters_timeout():
 	if get_visible_characters() > get_total_character_count():
-		if $Timer.is_stopped():
+		if $Timer.is_stopped() and skippedDialog == false:
 			$Timer.start()
 	else:
 		set_visible_characters(get_visible_characters() + 1)
-	
+
 func _on_Timer_timeout():
+	CheckForEndOfDialog()
+		
+func RefreshDialog():
 	if page < dialog.size() - 1:
 		page += 1
 		set_bbcode(dialog[page])
 		set_visible_characters(0)
+
+func CheckForEndOfDialog():
+	if dialog.size() - 1 == page:
+		get_parent().hide()
+	RefreshDialog()
